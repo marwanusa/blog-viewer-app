b957ae461d13afa856bf79fcc68225a00943eaae
marwanusa <professionalma42@gmail.com>
2025-06-28 16:58:04 +0300

    refactor: restructure GridPostCard component for improved readability and maintainability

--- src/components/posts/GridPostCard.tsx
+++ src/components/posts/GridPostCard.tsx
@@ -9,30 +9,37 @@ type Props = {
 
 export default function GridPostCard({ post }: Props) {
   return (
-    <div className="relative group cursor-pointer">
-      <Link href={`/posts/${post.id}`}>
-        <div className="bg-white dark:bg-[#10172A] rounded-xl shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300" >
-
-          <Image
-            src={`https://picsum.photos/seed/${post.id}/1200/500`}
-            alt={post.title}
-            width={1200}
-            height={500}
-            className="w-full h-48 object-cover"
-          />
-          <div className="p-4">
-            <p className="text-xs text-gray-500 dark:text-gray-400 mb-1">Sunday, 1 Jan 2023</p>
-            <h3 className="text-lg font-semibold text-black dark:text-white mb-1 whitespace-nowrap overflow-hidden text-ellipsis">
-              {post.title}
-            </h3>
-            <p className="text-sm text-gray-700 dark:text-gray-300 line-clamp-2">
-              {post.body}
-            </p>
-            <PostTags/>
+<div className="relative group cursor-pointer md:max-w-full max-w-[350px] mx-auto sm:mx-0">
+      <div className="bg-white dark:bg-[#10172A] rounded-xl shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 w-full ">
+        <Link href={`/posts/${post.id}`}>
+          <div>
+            <Image
+              src={`https://picsum.photos/seed/${post.id}/1200/500`}
+              alt={post.title}
+              width={1200}
+              height={500}
+              className="w-full h-48 object-cover"
+            />
           </div>
+        </Link>
+
+        <div className="p-4">
+          <p className="text-xs text-gray-500 dark:text-gray-400 mb-1">Sunday, 1 Jan 2023</p>
+          <h3 className="text-lg font-semibold text-black dark:text-white mb-1 whitespace-nowrap overflow-hidden text-ellipsis">
+            {post.title}
+          </h3>
+          <p className="text-sm text-gray-700 dark:text-gray-300 line-clamp-2">
+            {post.body}
+          </p>
+          <PostTags />
         </div>
-        <div className="absolute inset-0 bg-pink-500/40 dark:bg-purple-700/30 backdrop-blur-sm rounded-xl flex items-center justify-center h-0 group-hover:h-[100%] transition-all duration-300">
-          <span className="text-white text-lg font-semibold opacity-0 group-hover:opacity-100 transition-all duration-300">Go to full post →</span>
+      </div>
+
+      <Link href={`/posts/${post.id}`} className="absolute inset-0 rounded-xl">
+        <div className="bg-pink-500/40 dark:bg-purple-700/30 backdrop-blur-sm flex items-center justify-center h-0 group-hover:h-full transition-all duration-300 rounded-xl">
+          <span className="text-white text-lg font-semibold opacity-0 group-hover:opacity-100 transition-all duration-300">
+            Go to full post →
+          </span>
         </div>
       </Link>
     </div>
